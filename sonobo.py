eb95b472b647f1a02d2a68bb48147c66a39309e1
Matt Jones <jonesmr@gmail.com>
2021-12-27 20:27:19 +0000

    repeat-key deduping and some crude exception handling

--- sonobo.py
+++ sonobo.py
@@ -5,6 +5,7 @@
 # - Support multi-room joining
 
 import cgi
+import datetime
 import http.server
 import json
 import shutil
@@ -16,6 +17,16 @@ import time
 import urllib.parse
 
 EVENT_DEVICE_PATH = '/dev/input/by-id/usb-Telink_Wireless_Receiver-if01-event-kbd'
+
+EV_KEY = 0x01
+KEY_UP = 103
+KEY_DOWN = 108
+KEY_LEFT = 105
+KEY_RIGHT = 106
+KEY_SPACE = 57
+KEY_BACKSPACE = 14
+
+KEY_F12 = 88
 
 KEY_STRING_TO_CODE_MAP = {
     '1': 2,
@@ -76,10 +87,15 @@ class Sonobo:
     mutex = threading.Lock()
     speaker = None
 
+    last_key = None
+    last_key_timestamp = None
+
     def __init__(self, songmap_json, speaker):
         self.songmap_json = songmap_json
         self.key_code_to_song_map = songmap_json_to_map(songmap_json)
         self.speaker = speaker
+        self.last_key = -1
+        self.last_key_timestamp = datetime.datetime.min
 
     def get_songmap_json(self):
         self.mutex.acquire()
@@ -113,113 +129,124 @@ class Sonobo:
     def coordinator(self):
         return self.speaker.group.coordinator
 
+    def get_keypress(self, f):
+        # https://www.kernel.org/doc/Documentation/input/input.txt
+        #
+        # Section5: Event interfaces
+        #
+        # You can use blocking and nonblocking reads, also select() on the
+        # /dev/input/eventX devices, and you'll always get a whole number of input
+        # events on a read. Their layout is:
+        #
+        # struct input_event {
+        #	struct timeval time;
+        #	unsigned short type;
+        #	unsigned short code;
+        #	unsigned int value;
+        # };
+        #
+        # 'struct timeval' is from <sys/time.h> and is:
+        # struct timeval {
+        #	long	tv_sec;		/* seconds */
+        #	long	tv_usec;	/* and microseconds */
+        # };
+
+        struct_format = 'llHHi'  # long, long, short, short, int
+        size = struct.calcsize(struct_format)
+        data = f.read(size)
+
+        _tv_sec, _tv_usec, typet, code, value = struct.unpack(struct_format, data)
+
+        # 'time' is the timestamp, it returns the time at which the event happened.
+        # Type is for example EV_REL for relative moment, EV_KEY for a keypress or
+        # release. More types are defined in include/uapi/linux/input-event-codes.h.
+        #
+        # 'code' is event code, for example REL_X or KEY_BACKSPACE, again a complete
+        # list is in include/uapi/linux/input-event-codes.h.
+        #
+        # 'value' is the value the event carries. Either a relative change for
+        # EV_REL, absolute new value for EV_ABS (joysticks ...), or 0 for EV_KEY for
+        # release, 1 for keypress and 2 for autorepeat.
+        #
+        # https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes
+
+        return (typet, code, value)
+
+    def dispatch(self, typet, code, value):
+        if typet == EV_KEY and value == 1:
+            # Keypress
+            print("%d pressed" % (code))
+            fast_repeat = False
+            if self.last_key == code:
+                delay = datetime.datetime.now() - self.last_key_timestamp
+                if delay.total_seconds() < 2.0:
+                    fast_repeat = True
+
+            if code == KEY_SPACE:
+                if self.coordinator().get_current_transport_info()['current_transport_state'] != 'PLAYING':
+                    print("Play")
+                    self.coordinator().play()
+                else:
+                    print("Pause")
+                    self.coordinator().pause()
+            elif code == KEY_BACKSPACE:
+                print("Pause")
+                self.coordinator().pause()
+            elif code == KEY_UP:
+                current_vol = self.coordinator().volume
+                print("Volume up (@%d)" % current_vol)
+                if self.coordinator().volume > 15:
+                    print("Volume capped")
+                else:
+                    self.coordinator().set_relative_volume(2)
+            elif code == KEY_DOWN:
+                print("Volume down")
+                self.coordinator().set_relative_volume(-2)
+            elif code == KEY_RIGHT:
+                print("Next")
+                self.coordinator().next()
+            elif code == KEY_LEFT:
+                print("Previous")
+                self.coordinator().previous()
+            elif code == KEY_F12:
+                print("=== Dumping Sonos Playlist IDs ===")
+                for playlist in self.coordinator().get_sonos_playlists():
+                    print("title=%s item_id=%s" % (playlist.title, playlist.item_id))
+            elif song := self.song_for_code(code):
+                if fast_repeat:
+                    print("Ignoring fast-repeat of %d" % code)
+                else:
+                    print('Song %s' % song)
+                    if song.kind == 'SPOTIFY':
+                        self.coordinator().clear_queue()
+                        living_room_sharelink = soco.plugins.sharelink.ShareLinkPlugin(self.coordinator())
+                        living_room_sharelink.add_share_link_to_queue(song.payload)
+                        self.coordinator().play()
+                    elif song.kind == 'SONOS_PLAYLIST_NAME':
+                        playlist = self.coordinator().get_sonos_playlist_by_attr(
+                            'title', song.payload)
+                        self.coordinator().clear_queue()
+                        self.coordinator().add_to_queue(playlist);
+                        self.coordinator().play()
+                    else:
+                        print('unknown song kind: %s' % song.kind)
+
+            self.last_key = code
+            self.last_key_timestamp = datetime.datetime.now()
+
     def loop(self):
         print('opening "%s"' % (EVENT_DEVICE_PATH))
         with open(EVENT_DEVICE_PATH, 'rb') as f:
             print('READY')
             while True:
-                # https://www.kernel.org/doc/Documentation/input/input.txt
-                #
-                # Section5: Event interfaces
-                #
-                # You can use blocking and nonblocking reads, also select() on the
-                # /dev/input/eventX devices, and you'll always get a whole number of input
-                # events on a read. Their layout is:
-                #
-                # struct input_event {
-                #	struct timeval time;
-                #	unsigned short type;
-                #	unsigned short code;
-                #	unsigned int value;
-                # };
-                #
-                # 'struct timeval' is from <sys/time.h> and is:
-                # struct timeval {
-                #	long	tv_sec;		/* seconds */
-                #	long	tv_usec;	/* and microseconds */
-                # };
-
-                struct_format = 'llHHi'  # long, long, short, short, int
-                size = struct.calcsize(struct_format)
-                data = f.read(size)
-
-                _tv_sec, _tv_usec, typet, code, value = struct.unpack(struct_format, data)
-
-                # 'time' is the timestamp, it returns the time at which the event happened.
-                # Type is for example EV_REL for relative moment, EV_KEY for a keypress or
-                # release. More types are defined in include/uapi/linux/input-event-codes.h.
-                #
-                # 'code' is event code, for example REL_X or KEY_BACKSPACE, again a complete
-                # list is in include/uapi/linux/input-event-codes.h.
-                #
-                # 'value' is the value the event carries. Either a relative change for
-                # EV_REL, absolute new value for EV_ABS (joysticks ...), or 0 for EV_KEY for
-                # release, 1 for keypress and 2 for autorepeat.
-                #
-                # https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
-                EV_KEY = 0x01
-                KEY_UP = 103
-                KEY_DOWN = 108
-                KEY_LEFT = 105
-                KEY_RIGHT = 106
-                KEY_SPACE = 57
-                KEY_BACKSPACE = 14
-
-                KEY_F12 = 88
-
-
-                if typet == EV_KEY and value == 1:
-                    # Keypress
-                    print("%d pressed" % (code))
-                    if code == KEY_BACKSPACE:
-                        print("Pause")
-                        self.coordinator().pause();
-                    if code == KEY_SPACE:
-                        if self.coordinator().get_current_transport_info()['current_transport_state'] != 'PLAYING':
-                            print("Play")
-                            self.coordinator().play()
-                        else:
-                            print("Pause")
-                            self.coordinator().pause()
-                    elif code == KEY_BACKSPACE:
-                        print("Pause")
-                        self.coordinator().pause()
-                    elif code == KEY_UP:
-                        current_vol = self.coordinator().volume
-                        print("Volume up (@%d)" % current_vol)
-                        if self.coordinator().volume > 15:
-                            print("Volume capped")
-                        else:
-                            self.coordinator().set_relative_volume(2)
-                    elif code == KEY_DOWN:
-                        print("Volume down")
-                        self.coordinator().set_relative_volume(-2)
-                    elif code == KEY_RIGHT:
-                        print("Next")
-                        self.coordinator().next()
-                    elif code == KEY_LEFT:
-                        print("Previous")
-                        self.coordinator().previous()
-                    elif code == KEY_F12:
-                        print("Dumping Sonos Playlist IDs")
-                        for playlist in self.coordinator().get_sonos_playlists():
-                            print("title=%s item_id=%s" % (playlist.title, playlist.item_id))
-                    elif song := self.song_for_code(code):
-                        print('Song %s' % song)
-                        if song.kind == 'SPOTIFY':
-                            self.coordinator().clear_queue()
-                            living_room_sharelink = soco.plugins.sharelink.ShareLinkPlugin(self.coordinator())
-                            living_room_sharelink.add_share_link_to_queue(song.payload)
-                            self.coordinator().play()
-                        elif song.kind == 'SONOS_PLAYLIST_NAME':
-                            playlist = self.coordinator().get_sonos_playlist_by_attr(
-                                'title', song.payload)
-                            self.coordinator().clear_queue()
-                            self.coordinator().add_to_queue(playlist);
-                            self.coordinator().play()
-                        else:
-                            print('unknown song kind: %s' % song.kind)
-
+                try:
+                    typet, code, value = self.get_keypress(f)
+                    self.dispatch(typet, code, value)
+                except Exception as e:
+                    print("===== EXCEPTION: ", datetime.datetime.now(), " =====")
+                    print(type(e))
+                    print(e.args)
+                    print(e)
 
 def speaker_with_name(speakers, name):
     for speaker in speakers:
@@ -234,13 +261,15 @@ def songmap_json_to_map(json_songmap_contents) -> dict[str, SongInfo]:
     return key_code_to_song_map
 
 
-class SonoboHTTPHandler(http.server.BaseHTTPRequestHandler):
+class SonoboHTTPHandler(http.server.SimpleHTTPRequestHandler):
     sonobo: Sonobo = None
     json_songmap = None
 
     def __init__(self, sonobo: Sonobo, *args):
         self.sonobo = sonobo
-        http.server.BaseHTTPRequestHandler.__init__(self, *args)
+#        http.server.BaseHTTPRequestHandler.__init__(self, *args)
+        super().__init__(*args)
+
 
     def do_GET(self):
         print('do_GET')
