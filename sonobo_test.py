6586ea6c418b4e05703031fe963d92236c922000
Matt Jones <jonesmr@gmail.com>
2021-12-30 03:00:29 +0000

    another sonobo_test case

--- sonobo_test.py
+++ sonobo_test.py
@@ -1,11 +1,25 @@
 import json
+import logging
+import sys
 import unittest
 import unittest.mock
 
 import sonobo
 
+logger = logging.getLogger()
+logger.level = logging.DEBUG
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
+
+class FakeAvTransport:
+    foo = None
+
 class FakeCoordinator:
     playing = False
+
+    def __init__(self):
+        self.playing = False
+        self.avTransport = FakeAvTransport()
 
     def get_current_transport_info(self):
         return {'current_transport_state': 'PLAYING' if self.playing else 'STOPPED'}
@@ -17,27 +31,45 @@ class FakeCoordinator:
         self.playing = False
 
 class FakeGroup:
-    coordinator = FakeCoordinator()
+    def __init__(self):
+        self.coordinator = FakeCoordinator()
+
 
 class FakeSpeaker:
-    group = FakeGroup()
+    def __init__(self):
+        self.group = FakeGroup()
 
-class TestSonobo(unittest.TestCase):
-    def test_foo(self):
-        songmap_string = """[
+ONE_SONG_RAW_SONG_MAP = """[
    {
         "debugName": "Atencion Atencion - Que Pasa Con La Music",
         "key": "A",
         "kind": "SPOTIFY",
-        "payload": "https://open.spotify.com/album/6gTdDnREfXIGjEKqfsBWPi?si=9N6YIv0UTluLn01_ZkMyMg"
+        "payload": "https://open.spotify.com/track/payload"
     }
 ]"""
 
-        speaker = FakeSpeaker
+class TestSonobo(unittest.TestCase):
+    def test_play_from_song_map(self):
+        speaker = FakeSpeaker()
+        speaker.group.coordinator.clear_queue = unittest.mock.MagicMock()
+        speaker.group.coordinator.avTransport.AddURIToQueue = unittest.mock.MagicMock()
+        speaker.group.coordinator.play = unittest.mock.MagicMock(wraps=speaker.group.coordinator.play)
+
+        songmap_json = json.loads(ONE_SONG_RAW_SONG_MAP)
+        s = sonobo.Sonobo(songmap_json, speaker)
+
+        s.dispatch(sonobo.EV_KEY, sonobo.KEY_STRING_TO_CODE_MAP['A'], 1)
+
+        speaker.group.coordinator.clear_queue.assert_called_once()
+        speaker.group.coordinator.avTransport.AddURIToQueue.assert_called_once()  ## XXX Get URL
+        speaker.group.coordinator.play.assert_called_once()
+
+    def test_play_pause(self):
+        speaker = FakeSpeaker()
         speaker.group.coordinator.play = unittest.mock.MagicMock(wraps=speaker.group.coordinator.play)
         speaker.group.coordinator.pause = unittest.mock.MagicMock(wraps=speaker.group.coordinator.pause)
 
-        songmap_json = json.loads(songmap_string)
+        songmap_json = json.loads(ONE_SONG_RAW_SONG_MAP)
         s = sonobo.Sonobo(songmap_json, speaker)
 
         s.dispatch(sonobo.EV_KEY, sonobo.KEY_SPACE, 1)
